d58a9345e3026da48dedc48f8915bcfb71c4697d
Varun <varunoshawa@gmail.com>
2025-08-26 02:07:08 -0400

    updated the structure of the project and added nodemon as a dependency

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-require('dotenv').config({ path: '../.env' });
+require('dotenv').config();
 
 const app = express();
 
@@ -9,8 +9,15 @@ const PORT = process.env.PORT || 8080;
 console.log('PORT:', process.env.PORT);
 
 app.get('/', (req, res) => {
-    res.send('Server for amortize api is running!');
+    res.status(200).send('Server for amortize api is running!');
 });
+
+
+app.post('/v1/submit/', (req, res) => {
+    console.log("received a POST request to /v1/submit" + req.params.ingredient);
+
+
+})
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
